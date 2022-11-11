90c2f0998540ec30ecc1825c6b98d780d3f2c338
bitzess110 <bitzees110@gmail.com>
2022-11-11 18:59:45 +0530

    K

---
+++ APE_COIN/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = require("./router/routes");
+const app = express();
+// we use JSON  for making API
+app.use(bodyParser.json())
+
+
+const port = process.env.port
+
+// we use JSON  for making API
+app.use(express.json())
+app.use('/user',router)
+// Load Routes
+
+//set app port
+app.listen(1010, () => {
+  console.log(`APE_COIN server listening at http://localhost:${1010}`);
+});
