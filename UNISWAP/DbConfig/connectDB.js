d38ab53bf3edf0fe1551258fa7326fd60f1c506c
bitzess110 <bitzees110@gmail.com>
2022-11-11 18:58:37 +0530

    k

---
+++ UNISWAP/DbConfig/connectDB.js
@@ -0,0 +1,22 @@
+//Connect to the database
+const mysql = require('mysql');
+
+const mysqlConnection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"Salman@110",
+    database:"crypto_blockchain_db"
+  });
+  
+  //Connect to the database
+  mysqlConnection.connect((err)=>{
+    if(err){
+      console.warn("error")
+    }
+    else
+    {
+      console.warn("connected")
+    }
+  });
+
+ module.exports = mysqlConnection
